98b206ed99dc65bdf8203a382672396fe9f66e02
Justin Lee <147962090+JustinCoKA@users.noreply.github.com>
2025-02-06 19:16:13 +1100

    Delete Data_Analyst directory

--- Data_Analyst/final_data_analyst/final_ver_data/scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Initialization file for the scripts module.
-Ensures that all script modules can be imported properly.
-"""
-
-# Import key functions from scripts for easier access
-from .final_preprocess import load_and_merge_data
-from .initial_preprocess import initial_data_preparation
-from .visualization import generate_visualizations
